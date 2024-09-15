facb6d9cdacaa19a0ffba975f32953f39d94a970
brederle <brederle.git@magenta.de>
2024-09-15 15:34:55 +0200

    INstall cncjs as dedicated cncjs user without shell access

--- cncjs/cncjs_install.sh
+++ cncjs/cncjs_install.sh
@@ -5,38 +5,42 @@ export LANGUAGE=${LOCALE}.${ENCODING}
 export LANG=${LOCALE}.${ENCODING}
 export LC_ALL=${LOCALE}.${ENCODING}
 
+export USERDIR=/home/${CNCUSER}
+
+# adapt rights of copied settingss
+chown ${CNCUSER}:${CNCUSER} /home/${CNCUSER}/.cncrc
 
 ###
 # Install cnc.js as extra cncjs CNCUSER
 #
-npm config set prefix ~/.npm
-echo "Installing cncjs packages for user " $(whoami)
+echo "Installing cncjs packages for user " ${CNCUSER}
+mkdir -p ${USERDIR}/.npm
+npm config set prefix ${USERDIR}/.npm
 npm_config_loglevel=silent npm install -g cncjs@${CNCJS_VERSION} --unsafe-perm
 # allow fqdn resolution in final image again
 
-
 ###
-# Install auto leveler
-#
-echo "Installing autoscaler for user" $(whoami)
-git clone -q https://github.com/kreso-t/cncjs-kt-ext.git ~/cncjs-kt-ext
-cd ~/cncjs-kt-ext
-npm_config_loglevel=silent npm install
-rm -rf ~/cncjs-kt-ext
+# cncjs service
+echo "Installing cncjs@.service"
+cat > /etc/systemd/system/cncjs@.service <<SERVICE_EOF
+[Unit]
+Description=CNC JS server
+After=network.target
 
-###
-# Install patched grblsim
-#
-echo "Installing grblsim for user" $(whoami)
-mkdir -p ~/grbl
-git clone -q https://github.com/gnea/grbl ~/grbl
-git clone -q https://github.com/grbl/grbl-sim ~/grbl/grbl/grbl-sim
-cd ~/grbl/grbl/grbl-sim
-# atm, some patching on rpi is needed
-curl -L https://github.com/grbl/grbl-sim/pull/29.patch | git apply
-# TODO: deliver real patch to project
-sed -ri 's/util\/floatunsisf.o//g' Makefile
-make new
-mv -i gvalidate.exe /usr/local/bin/gvalidate
-mv -i grbl_sim.exe /usr/local/bin/grblsim
-rm -rf ~/grbl
+[Service]
+Type=simple
+Environment=PATH=/home/%i/.npm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+WorkingDirectory=/home/%i
+ExecStart=/home/%i/.npm/bin/cncjs --host 0.0.0.0 -p 8000 --config /home/%i/.cncrc
+ExecReload=/bin/kill -HUP $MAINPID
+KillMode=process
+USER=%i
+Restart=on-failure
+RestartPreventExitStatus=255
+
+[Install]
+WantedBy=multi-user.target
+SERVICE_EOF
+
+# Enable cncjs service
+systemctl --quiet enable cncjs@${CNCUSER}.service
